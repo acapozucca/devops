129f33b50c50c67c545f26d32c68ed6c17e90dfe
Alfredo Capozucca <alfredo.capozucca@uni.lu>
2019-09-17 17:41:20 +0200

    Create dummy maven integration test project

---
+++ pipeline/s1-create-skeleton/ITMavenHelloWorldProject/scripts/run-integration-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ../../MavenHelloWorldProject/target/MavenHelloWorldProject-1.0-SNAPSHOT.jar ../target/
+
+java -jar ./../target/ITMavenHelloWorldProject-1.0-SNAPSHOT.jar
+
+
---
+++ pipeline/s1-create-skeleton/ITMavenHelloWorldProject/src/it/ITDummyTest.java
@@ -0,0 +1,28 @@
+package it;
+
+import com.jcg.maven.App;
+
+public class ITDummyTest {
+
+	public static void main(String[] args) {
+		
+		App it_myapp = new App();
+		
+		System.out.println("\nIntegration Testing");
+		
+		
+		System.out.println("\nCall 1");
+		it_myapp.method1();
+		
+		
+		System.out.println("\nCall 2");
+		it_myapp.method1();
+		
+		
+		System.out.println("\nCall 3");
+		it_myapp.method1();
+		
+		
+	}
+
+}
